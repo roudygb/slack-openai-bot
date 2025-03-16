a0ea239048e82533742e8ab84362b2ed7a80f6d0
Kyle Gallatin <kylegallatin@gmail.com>
2025-03-16 18:05:44 +0000

    update to use the repsonses api

--- bot.py
+++ bot.py
@@ -1,50 +1,41 @@
 import os
+from typing import List
 from openai import OpenAI
 from slack_bolt import App
 from dotenv import load_dotenv
-
-system_prompt =  '''
-You are a helpful bot.
-'''
 
 load_dotenv()
 
 SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN")
 SLACK_SIGNING_SECRET = os.environ.get("SLACK_SIGNING_SECRET")
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+INSTRUCTIONS =  '''
+You are a helpful bot.
+'''
 
 client = OpenAI(api_key=OPENAI_API_KEY)
-
 app = App(
     token=SLACK_BOT_TOKEN,
     signing_secret=SLACK_SIGNING_SECRET
 )
 
+def process_response_output(output: List) -> str:
+    for response_object in output:
+        if type(response_object) == ResponseOutputMessage:
+            return response_object.content[0].text
+
 @app.event("app_mention")
 def handle_mention(event, say):
     text = event["text"]
     try:
-        response = client.chat.completions.create(
-            model="gpt-4o-mini",
-            messages = [
-                {
-                "role": "system",
-                "content": [
-                    {
-                    "type": "text",
-                    "text": system_prompt
-                    }
-                ]
-                },
-                {"role": "user",
-                "content": [{
-                    "type": "text",
-                    "text": text
-                }]
-                }
-            ]
+        response = client.responses.create(
+            model="gpt-4o",
+            tools=[{"type": "web_search_preview"}],
+            tool_choice="auto",
+            input=INPUT,
+            instructions=INSTRUCTIONS
         )
-        say(response.choices[0].message.content)
+        say(process_response_output(response.output))
     except Exception as e:
         say(f"An error occurred: {e}")
 
